9a196ebddaf460e8fcefd13d8187d5b37dcc46b3
Alexander Björk <alexander-bjork@hotmail.com>
2024-12-03 14:17:34 +0200

    Update day3.py

--- 2024/day3.py
+++ 2024/day3.py
@@ -1,16 +1,41 @@
 import csv
 import re
 
-with open('day3.txt', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ')
-    y=0
-    for row in spamreader:
-        newnum=[]
-        num = list(re.findall(r'mul(\(\d+,\d+)\)', str(row)))
-        i=0
-        while i < len(num):
-            newnum.append(num[i].replace('(', ''))
-            x = str(newnum[i]).split(',')
-            y += int(x[0])*int(x[1])
-            i += 1
-    print(y)
+def part1():
+    with open('day3.txt', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ')
+        y=0
+        for row in spamreader:
+            newnum=[]
+            reg = re.compile('mul(\(\d+.\d+)\)')
+            num = re.findall(reg, str(row))
+            i=0
+            while i < len(num):
+                newnum.append(num[i].replace('(', ''))
+                x = str(newnum[i]).split(',')
+                y += int(x[0])*int(x[1])
+                i += 1
+        print(y)
+
+def part2():
+    with open('day3.txt', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ')
+        y=0
+        for row in spamreader:
+            newnum=[]
+            reg = re.compile("(mul\(\d+,\d+\)|don't\(\)|do\(\))")
+            num = re.findall(reg, str(row))
+            i=0
+            while i < len(num):
+                newnum.append(num[i].replace('(', ''))
+                newnum[i] = newnum[i].replace(')', '')
+                newnum[i]=newnum[i].replace('m', '')
+                newnum[i]=newnum[i].replace('u', '')
+                newnum[i]=newnum[i].replace('l', '')
+                x = str(newnum[i]).split(',')
+                y += int(x[0])*int(x[1])
+                i += 1
+        print(y)
+
+part1()
+part2()
